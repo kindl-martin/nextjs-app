c2fa83b673c3828891d60c9555ff531f27729e0c
Martin Kindl <kindl.m@icloud.com>
2025-07-21 16:33:41 +0200

    add reorder on tasks

--- app/ui/tasks/task.tsx
+++ app/ui/tasks/task.tsx
@@ -14,41 +14,15 @@ import { use, useEffect, useState } from 'react';
 import clsx from 'clsx';
 import { EllipsisVerticalIcon, TrashIcon } from '@heroicons/react/24/outline';
 import { Button } from '@heroui/button';
-import { Reorder } from 'motion/react';
-
-export function Task({ task }: { task: tasks & { timer: timer[] } }) {
-  const openTimer = task.timer.find((t) => !t.end);
-  const totalTime = getTotalTime(task.timer);
-  const isDone = task.state === task_state.DONE;
-
-  return (
-    <Card
-      className="flex-row items-center justify-between gap-2 p-5"
-      key={task.id}
-    >
-      <EllipsisVerticalIcon
-        data-swapy-handle={task.id}
-        className="h-5 w-5 flex-shrink-0 cursor-grab"
-      />
-      <Checkbox
-        size="lg"
-        isSelected={isDone}
-        onValueChange={() =>
-          toggleTaskState(task.id, isDone ? task_state.OPEN : task_state.DONE)
-        }
-      />
-      <h3 className={clsx('flex-grow', { 'line-through': isDone })}>
-        {task.name}
-      </h3>
-      <Stopwatch timer={openTimer} totalTime={totalTime} taskId={task.id} />
-      <Button
-        onPress={() => deleteTask(task.id)}
-        startContent={<TrashIcon className="h-4 w-4" />}
-        variant="ghost"
-      />
-    </Card>
-  );
-}
+import {
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+} from '@heroui/modal';
+import { Reorder, useDragControls } from 'motion/react';
+import { useDisclosure } from '@heroui/use-disclosure';
 
 export function TaskList({
   tasks,
@@ -67,16 +41,72 @@ export function TaskList({
       axis="y"
       values={taskList}
       onReorder={(newOrder) => {
-        orderTasks(newOrder);
+        void orderTasks(newOrder);
         setTaskList(newOrder);
       }}
       className="mt-10 flex flex-col gap-4"
     >
       {taskList.map((task) => (
-        <Reorder.Item key={task.id} value={task}>
-          <Task task={task} />
-        </Reorder.Item>
+        <Task key={task.id} task={task} />
       ))}
     </Reorder.Group>
+  );
+}
+
+export function Task({ task }: { task: tasks & { timer: timer[] } }) {
+  const openTimer = task.timer.find((t) => !t.end);
+  const totalTime = getTotalTime(task.timer);
+  const isDone = task.state === task_state.DONE;
+
+  const controls = useDragControls();
+
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+  return (
+    <Reorder.Item value={task} dragListener={false} dragControls={controls}>
+      <Card
+        className="flex-row items-center justify-between gap-2 p-5"
+        key={task.id}
+      >
+        <EllipsisVerticalIcon
+          onPointerDown={(e) => controls.start(e)}
+          className="h-5 w-5 flex-shrink-0 cursor-grab"
+        />
+        <Checkbox
+          size="lg"
+          isSelected={isDone}
+          onValueChange={() =>
+            toggleTaskState(task.id, isDone ? task_state.OPEN : task_state.DONE)
+          }
+        />
+        <h3 className={clsx('flex-grow', { 'line-through': isDone })}>
+          {task.name}
+        </h3>
+        <Stopwatch timer={openTimer} totalTime={totalTime} taskId={task.id} />
+        <Button
+          onPress={onOpen}
+          startContent={<TrashIcon className="h-4 w-4" />}
+          variant="ghost"
+        />
+        <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+          <ModalContent>
+            {(onClose) => (
+              <>
+                <ModalHeader className="flex flex-col gap-1">
+                  Delete task
+                </ModalHeader>
+                <ModalBody>Are you sure you want to delete the task?</ModalBody>
+                <ModalFooter>
+                  <Button onPress={onClose}>Cancel</Button>
+                  <Button color="danger" onPress={() => deleteTask(task.id)}>
+                    Delete
+                  </Button>
+                </ModalFooter>
+              </>
+            )}
+          </ModalContent>
+        </Modal>
+      </Card>
+    </Reorder.Item>
   );
 }
