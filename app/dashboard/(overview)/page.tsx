c6adb8a42c337c9fe0cdf6d9826313c0ad2df43f
Martin Kindl <kindl.m@icloud.com>
2025-09-14 22:30:31 +0200

    action

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -11,6 +11,17 @@ export default function Page() {
     <main className="relative h-[500vh]">
       <section className="mx-auto flex w-full max-w-3xl flex-col justify-center">
         <FadeUp>
+          <motion.div
+            drag
+            style={{
+              width: 100,
+              height: 100,
+              backgroundColor: '#dd00ee',
+              borderRadius: 10,
+            }}
+          />
+        </FadeUp>
+        <FadeUp>
           <div className="example-container flex flex-col items-center justify-center gap-6 [--color-var:red]">
             <div
               className="box h-[100px] w-[100px] rounded-xl bg-[var(--color-var,yellow)] duration-300"
--- app/dashboard/profile/actions.tsx
+++ app/dashboard/profile/actions.tsx
@@ -1,11 +1,14 @@
 'use server';
 
 import { prisma } from '@/app/lib/db';
+import { auth } from '@/auth';
 
 export async function updateProfile() {
+  const session = await auth();
+
   await prisma.users.update({
     where: {
-      id: 'ed3869ba-7c54-4cf1-b4ba-6f41a7fc89d6',
+      id: session?.user?.id,
     },
     data: {
       phone: '+420735253288',
