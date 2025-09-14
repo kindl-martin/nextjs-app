c9e4b67f2517687410e2eb19a42e8a4a373bfeb6
Martin Kindl <kindl.m@icloud.com>
2025-09-14 22:45:19 +0200

    add profile edit

--- app/dashboard/profile/_components/form.tsx
+++ app/dashboard/profile/_components/form.tsx
@@ -1,25 +1,49 @@
 'use client';
 
 import { roles, users } from '@/app/generated/prisma';
-import { use } from 'react';
+import { use, useActionState } from 'react';
 import { Avatar } from '@heroui/avatar';
 import { Card } from '@heroui/card';
 import { Button } from '@heroui/button';
 import { updateProfile } from '../actions';
+import { Input } from '@heroui/input';
+import { Form } from '@heroui/form';
+import { addToast } from '@heroui/toast';
+import { ErrorState } from '@/app/lib/types';
 
 export function UserForm({
   user,
 }: {
   user: Promise<users & { roles: roles[] }>;
 }) {
-  const { image, name, email } = use(user);
+  const { image, name, email, phone } = use(user);
+
+  const [, formAction, isPending] = useActionState(
+    async (prevState: ErrorState | undefined, formData: FormData) => {
+      const response = await updateProfile(prevState, formData);
+      if (response?.error) {
+        addToast({
+          title: response.error.title,
+          description: response.error.description,
+          color: 'danger',
+        });
+      }
+      return response;
+    },
+    undefined,
+  );
 
   return (
     <Card className="w-fit p-6">
-      <Avatar src={image?.toString()} />
-      <p>{name}</p>
-      <p>{email}</p>
-      <Button onPress={updateProfile}>Edit</Button>
+      <Form action={formAction}>
+        <Avatar src={image?.toString()} />
+        <Input name="name" defaultValue={name ?? ''} placeholder="Name" />
+        <Input name="email" defaultValue={email ?? ''} placeholder="Email" />
+        <Input name="phone" defaultValue={phone ?? ''} placeholder="Phone" />
+        <Button type="submit" isLoading={isPending}>
+          Edit
+        </Button>
+      </Form>
     </Card>
   );
 }
--- app/dashboard/profile/actions.tsx
+++ app/dashboard/profile/actions.tsx
@@ -2,16 +2,36 @@
 
 import { prisma } from '@/app/lib/db';
 import { auth } from '@/auth';
+import { ErrorState } from '@/app/lib/types';
+import { revalidatePath } from 'next/cache';
 
-export async function updateProfile() {
+export async function updateProfile(
+  prevState: ErrorState | undefined,
+  formData: FormData,
+) {
   const session = await auth();
+  const { name, email, phone } = Object.fromEntries(formData.entries());
 
-  await prisma.users.update({
-    where: {
-      id: session?.user?.id,
-    },
-    data: {
-      phone: '+420735253288',
-    },
-  });
+  try {
+    await prisma.users.update({
+      where: {
+        id: session?.user?.id,
+      },
+      data: {
+        ...(name && { name: name.toString() }),
+        ...(email && { email: email.toString() }),
+        ...(phone && { phone: phone.toString() }),
+      },
+    });
+  } catch (error) {
+    console.error('Failed to update profile:', error);
+    return {
+      error: {
+        title: 'Database Error',
+        description: 'Failed to update profile.',
+      },
+    };
+  }
+
+  revalidatePath('/dashboard/profile');
 }
